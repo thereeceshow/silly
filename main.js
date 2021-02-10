2d1f21ee52dd64536dcbd5f8d626cf18221b6a16
Reece Walter <reece@reecewalter.com>
2021-02-10 17:50:13 -0500

    all working except custom UK

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 // 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
+const customWeight = document.getElementById('customweight');
+const customTemp = document.getElementById('customtemp');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
 }
+
+let weight = 300
+let temp = 94
+
+// I had to move these variables out of the functions there were created in
+// This is due to Scope.  I believe they are now in the Global Scope.
+// Basically if I define a var inside a function, outside of it, its unknown.
+// This also means two diff functions could have a var with the same name and dif values.
 
 // 2. RAW TEXT STRINGS
 
 // These create the elements of our story.  The first string is always used.
 // The next arrays hold the values that will later be randomly selected.
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = 'It was ' + temp + 'º fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs ' + weight + ' pounds, and it was a hot day.';
 
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 
@@ -52,9 +62,34 @@ function result() {
 
   }
 
+
+  if(customWeight.value !== '') {
+    weight = customWeight.value;
+    newStory = newStory.replace('300',weight);
+
+  }
+
+  if(customTemp.value !== '') {
+    temp = customTemp.value;
+    newStory = newStory.replace('94',temp);
+
+  }
+
+
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
+    // let weight = Math.round(300); COMMENTED THIS ONE OUT TO TRY TO STRETCH
+
+    // 1 lbs = .0714286 Stone
+    // Or divide pound by 14 -- LBS/14 = Stone
+    let stone = Math.round(weight/14);
+    // let temperature =  Math.round(94); STREEEEEEETCH GOOOOOOAAAAAALLLLLLLSSS
+    // (xºF - 32)*(5/9) = yºC
+    let cent = Math.round((temp - 32) * (5/9));
+    // replace these values in the Story
+    newStory = newStory.replace('300 pounds',stone + ' stone');
+    newStory = newStory.replace('94º fahrenheit',cent + 'º centagrade');
+    
+
 
   }
 
